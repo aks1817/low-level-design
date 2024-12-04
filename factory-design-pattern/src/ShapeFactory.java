872ac886d086124f7a82d04740cb7440ecb529c3
aks1817 <aks76625@gmail.com>
2024-12-04 15:21:51 +0530

    factory pattern

---
+++ factory-design-pattern/src/ShapeFactory.java
@@ -0,0 +1,12 @@
+public class ShapeFactory {
+    Shape getShape(String shapeName) {
+        switch (shapeName) {
+            case "Rectangle":
+                return new Rectangle();
+            case "Circle":
+                return new Circle();
+            default:
+                return null;
+        }
+    }
+}
